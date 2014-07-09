ddc6198608c90e877bfeb505e12056af10f7e655
Raymond Jacobson <me@raymondjacobson.com>
2014-07-08 19:43:00 -0700

    initial server

--- app.go
+++ app.go
@@ -1,7 +1,19 @@
 package main
 
-import "net/http"
+import (
+  "io"
+  "log"
+  "net/http"
+)
+
+func index(res http.ResponseWriter, req *http.Request) {
+  io.WriteString(res, "Hello, world!")
+}
 
 func main() {
-  
+  http.HandleFunc("/", index)
+  err := http.ListenAndServe(":1298", nil)
+  if err != nil {
+    log.Fatal("ListenAndServe: ", err)
+  }
 }
